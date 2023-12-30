412f44b6fde863e1b5aa82d45b679a7bc5daced8
LivingDemonness28 <ajamshamoodah@gmail.com>
2023-12-30 16:46:07 +0200

    2-selection_sort.c file updated 2nd

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * _swap - swap two ints in an array.
+ * @int_1: first int.
+ * @int_2: second int.
+ * Returns: Nothing.
+*/
+void _swap(int *int_1, int *int_2)
+{
+int hold;
+
+hold = *int_1;
+*int_1 = *int_2;
+*int_2 = hold;
+}
+
+/**
  * selection_sort - sort array of ints in asc order
  * @array: int array
  * @size: array size
